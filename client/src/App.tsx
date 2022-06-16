5cfca93a38d1e2c6a31048014c0e49dcf1165d18
Saurabh Dabas <developerdabas@gmail.com>
2022-06-16 19:01:50 +0530

    import interface and set props

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
-function App() {
+import axios from 'axios';
+import React, {useState,useEffect} from 'react';
+import  { Wrapper }  from './App.styles';
+import QuestionCard from './components/QuestionCard';
+import IQuestion from './interfaces/IQuestion';
+
+
+const App: React.FC= () => {
+
+  const [ question , setQuestion] = useState<string>("");
+  const [questions, setQuestions] = useState<IQuestion[]>([])
+  const [ questionNumber, setQuestionNumber] = useState<number>(0);
+  const [answers, setAnswers] = useState<string[]>([]);
+
+  useEffect(()=>{
+    axios.get("http://localhost:8080/quiz")
+    .then((response)=>{
+      setQuestions(response.data)
+      setQuestion(response.data[questionNumber].question)
+      setAnswers([...response.data[questionNumber].incorrect_answers,response.data[questionNumber].correct_answer])
+    })
+  },[])
   return (
-    <div className="App">
-      Quiz
-    </div>
+    <Wrapper>
+      <QuestionCard questions ={questions} questionNumber={questionNumber} question={question} setQuestion={setQuestion} answers={answers} setAnswers={setAnswers}/>
+    </Wrapper>
   );
 }
 

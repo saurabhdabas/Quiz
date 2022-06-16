cfb16575229da877a3342e9ff434e80519fee77c
Saurabh Dabas <developerdabas@gmail.com>
2022-06-16 23:32:42 +0530

    Add setAnswer prop to Question Card

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import React, {useState,useEffect} from 'react';
 import  { Wrapper }  from './App.styles';
 import QuestionCard from './components/QuestionCard';
 import IQuestion from './interfaces/IQuestion';
-import IAnswer from './interfaces/IAnswer';
+
 
 const App: React.FC= () => {
 

98e60993b9e20abb635950c480ac4bf56d0f873f
Saurabh Dabas <developerdabas@gmail.com>
2022-06-16 19:02:31 +0530

    Change styling

--- client/src/components/QuestionCard.styles.ts
+++ client/src/components/QuestionCard.styles.ts
@@ -1,1 +1,49 @@
 import styled from 'styled-components';
+
+export const QuestionWrapper = styled.div`
+  width: 900px;
+  height:30px;
+  background-image: url("https://img.freepik.com/free-photo/crumpled-yellow-paper-background-close-up_60487-2390.jpg?size=626&ext=jpg");
+  border-radius: 10px;
+  border: 2px solid #FFFFFF;
+  padding: 20px;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+  font-family: 'DM Sans', sans-serif;
+  text-align: center;
+  font-size:26px;
+`
+// type ButtonWrapperProps = {
+//   correct: boolean;
+//   userClicked: boolean;
+// }
+
+export const ButtonWrapper = styled.div`
+  transition: all 0.3s ease;
+
+  margin-top:50px;
+  display: grid;
+  width: 900px;
+  justify-items: center;
+  grid-column-gap: 40px;
+  grid-row-gap: 35px;
+  grid-template-columns: repeat(2,1fr);
+  grid-template-rows: repeat(2,1fr);
+
+
+  button {
+    cursor: pointer;
+    user-select: none;
+    font-size: 0.8rem;
+    width: 450px;
+    height:60px;
+    margin: 5px 0;
+    background-image: url("https://img.freepik.com/free-photo/crumpled-yellow-paper-background-close-up_60487-2390.jpg?size=626&ext=jpg");
+    border: 3px solid #ffffff;
+    box-shadow: 1px 2px 0px rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+    font-size:20px;
+    font-family: 'DM Sans', sans-serif;
+    color: #000;
+    text-shadow: 0px 1px 0px rgba(0, 0, 0, 0.25);
+  }
+`

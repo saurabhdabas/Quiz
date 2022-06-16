ad78cbcd243aa7c797deee5497e6c25eaa70f867
Saurabh Dabas <developerdabas@gmail.com>
2022-06-16 16:32:41 +0530

    Remove margin from question

---
+++ client/src/App.styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+width: 100vw;
+height: 100vh;
+opacity: 0.9;
+background-size:contain;
+// background: url('/background.jpg');
+
+p {
+  font-size: 1rem;
+  margin:0;
+}
+
+`;

1369593eb9b9db012a76907b3f690d9357f19057
Saurabh Dabas <developerdabas@gmail.com>
2022-06-17 21:17:38 +0530

    change styling

--- client/src/components/timer.styles.ts
+++ client/src/components/timer.styles.ts
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 export const TimerDesign = styled.div`
-  color:#35E9BC;
+  color:#FFF;
   width:80px;
   height:80px;
   font-family: 'DM Sans', sans-serif;
   font-size:50px;
   text-align:center;
-  background-color:#6948FE;
+  background-color:#FAB02B;
   border-radius:50px;
   padding:12px 5px 0 5px;
   > div{

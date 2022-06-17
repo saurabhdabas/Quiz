1adb6b315836757aa9dd8f361dff22b6463f71bc
Saurabh Dabas <developerdabas@gmail.com>
2022-06-18 01:50:17 +0530

    Add style to money grid

---
+++ client/src/components/MoneyGrid.styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Grid = styled.div`
+display:flex;
+flex-direction:row;
+align-items:center;
+justify-content:space-between;
+width: 300px;
+height:20px;
+color:#FFF;
+background-color: #6949FD;
+// background-image: url("https://img.freepik.com/free-photo/crumpled-yellow-paper-background-close-up_60487-2390.jpg?size=626&ext=jpg");
+border-radius: 10px;
+border: 2px solid #FFFFFF;
+padding: 20px;
+box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+font-family: 'DM Sans', sans-serif;
+text-align: center;
+font-size:20px;
+margin-bottom:5px;
+`

e8c536e0a8b037f6d745c213efef13f7c0bdf8f6
Saurabh Dabas <developerdabas@gmail.com>
2022-06-18 16:35:31 +0530

    change styling

---
+++ client/src/components/AmountCard.styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const AmountCard = styled.div`
+  position:absolute;
+> div {
+  position:relative;
+  top:-200px;
+  right:565px;
+  display:flex;
+  align-items:center;
+  justify-content:space-around;
+  border-radius:4px;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+  font-family: 'DM Sans', sans-serif;
+  width: 300px;
+  height:80px;
+  color:#FFF;
+  background-image: url("https://img.freepik.com/free-vector/green-crumpled-paper-texture-sheet-background_1017-37527.jpg?w=2000&t=st=1655536173~exp=1655536773~hmac=4556f1db4dfb69a8e8ae4d3a51744365fa8e83d38d89ea2b26900d187e44d344");
+}
+
+`
